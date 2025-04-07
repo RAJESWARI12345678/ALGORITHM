a4e17df1b2123d567b8af725ffbc43bc070627e2
RAJESWARI12345678 <109813798+RAJESWARI12345678@users.noreply.github.com>
2025-04-07 20:15:55 +0530

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 // function to swap the position of two elements
-void swap(int *a, int *b) {
-  int temp = *a;
+void swap(float *a, float *b) {
+  float temp = *a;
   *a = *b;
   *b = temp;
 }
 
 // function to perform the selection sort
-void selectionSort(int array[], int size) {
+void selectionSort(float array[], int size) {
   for (int step = 0; step < size - 1; step++) {
     int min_idx = step;
     for (int i = step + 1; i < size; i++) {
@@ -22,9 +22,9 @@ void selectionSort(int array[], int size) {
 }
 
 // function to print the array
-void printArray(int array[], int size) {
+void printArray(float array[], int size) {
   for (int i = 0; i < size; ++i) {
-    printf("%d  ", array[i]);
+    printf("%.2f  ", array[i]);  // Print with 2 decimal places
   }
   printf("\n");
 }
@@ -37,12 +37,12 @@ int main() {
   printf("Enter the number of elements: ");
   scanf("%d", &size);
 
-  int data[size];  // Declare the array with the user-defined size
+  float data[size];  // Declare the array with the user-defined size
 
   // Ask the user to input the elements of the array
-  printf("Enter %d integers:\n", size);
+  printf("Enter %d floating-point numbers:\n", size);
   for (int i = 0; i < size; ++i) {
-    scanf("%d", &data[i]);
+    scanf("%f", &data[i]);  // Accept float values
   }
 
   // Perform selection sort
